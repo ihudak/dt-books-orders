a601e5b4ac820a43fbd6750ade064604be18fcd1
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-30 14:39:51 +0200

    add count to version

--- src/main/java/com/dynatrace/orders/controller/VersionController.java
+++ src/main/java/com/dynatrace/orders/controller/VersionController.java
@@ -1,6 +1,8 @@
 package com.dynatrace.orders.controller;
 
 import com.dynatrace.orders.model.Version;
+import com.dynatrace.orders.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/version")
 public class VersionController {
+    @Autowired
+    private OrderRepository orderRepository;
     @Value("${service.version}")
     private String svcVer;
     @Value("${service.date}")
@@ -17,6 +21,6 @@ public class VersionController {
 
     @GetMapping("")
     public Version getVersion() {
-        return new Version("orders", svcVer, svcDate, "OK", "Healthy");
+        return new Version("orders", svcVer, svcDate, "OK", "Count: " + orderRepository.count());
     }
 }
